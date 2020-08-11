b8e9558788a0c55592fc34517dbc5561052da941
Imed <49333128+Imed27@users.noreply.github.com>
2020-08-11 17:56:36 +0200

    footer done with mdbreact

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -34,7 +34,7 @@ const Footer = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright:  MDBootstrap.com 
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
         </MDBContainer>
       </div>
     </MDBFooter>
